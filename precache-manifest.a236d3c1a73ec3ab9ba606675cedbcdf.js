73ccc492aa1d985976731bb38d762d09d56581eb
Sentenzos <49534576+Sentenzos@users.noreply.github.com>
2020-04-18 21:43:06 +0300

    Updates

---
+++ precache-manifest.a236d3c1a73ec3ab9ba606675cedbcdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fa66adeaa7f07abe8a21a53a38ae2b",
+    "url": "/%7Bbezier-curve%7D/index.html"
+  },
+  {
+    "revision": "1fa5ecdf9dfda73576e4",
+    "url": "/%7Bbezier-curve%7D/static/css/main.3b7d6c49.chunk.css"
+  },
+  {
+    "revision": "bbb1c8bd62d93213bf64",
+    "url": "/%7Bbezier-curve%7D/static/js/2.e46b7c0b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/%7Bbezier-curve%7D/static/js/2.e46b7c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa5ecdf9dfda73576e4",
+    "url": "/%7Bbezier-curve%7D/static/js/main.1fe6a961.chunk.js"
+  },
+  {
+    "revision": "9300187810d92f94ad1f",
+    "url": "/%7Bbezier-curve%7D/static/js/runtime-main.f4d240de.js"
+  },
+  {
+    "revision": "a82c4c3bfe6cd8a9d57597071772d0c7",
+    "url": "/%7Bbezier-curve%7D/static/media/Kanit-Regular.a82c4c3b.ttf"
+  }
+]);
